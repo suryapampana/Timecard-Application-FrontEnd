09231b7be24a92f90ea7053b13d04fe34acee745
suryapampana <17pa5a0320@vishnu.edu.in>
2021-06-09 19:31:07 +0530

    dropdown

--- src/common/AppNavBar.js
+++ src/common/AppNavBar.js
@@ -1,74 +1,141 @@
 import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { NavDropdown } from "react-bootstrap";
 
 export function AppNavBar() {
   return (
-    <Navbar
-      bg="primary"
-      expand="lg"
-      variant="light"
-      className="navbar sticky-top "
-    >
+    <Navbar bg="dark" expand="lg" variant="dark" className="navbar sticky-top ">
       <Navbar.Brand as={Link} to="/" className="text-white">
         TIME SHEET MANAGEMENT
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="navbar-dark" />
-      <Navbar.Collapse id="basic-navbar-nav" className="bg-white rounded m-2">
-        <Nav className="ml-auto">
-          <Nav.Link
-            as={Link}
-            to="/create-employee"
-            className="text-primary pl-2"
-          >
-            Create Employee
-          </Nav.Link>
-          <Nav.Link as={Link} to="/list-employee" className="text-primary pl-2">
-            List Employee
-          </Nav.Link>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="m-auto">
+          <NavDropdown title="Employee" id="nav-dropdown" className="nav-light">
+            <NavDropdown.Item
+              eventKey="4.1"
+              as={Link}
+              to="/create-employee"
+              className="pl-2"
+              style={{ color: "tomato" }}
+            >
+              Create Employee
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item
+              eventKey="4.2"
+              as={Link}
+              to="/list-employee"
+              className="pl-2"
+            >
+              Employee List
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+          </NavDropdown>
 
-          <Nav.Link
-            as={Link}
-            to="/create-supervisor"
-            className="text-primary pl-2"
+          <NavDropdown
+            title="Leave"
+            id="nav-dropdown"
+            className="color-primary"
           >
-            Create Supervisor
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="/list-supervisor"
-            className="text-primary pl-2"
-          >
-            List Supervisor
-          </Nav.Link>
+            <NavDropdown.Item
+              eventKey="4.1"
+              as={Link}
+              to="/apply-leave"
+              className="pl-2"
+              style={{ color: "tomato" }}
+            >
+              Apply Leave
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item
+              eventKey="4.2"
+              as={Link}
+              to="/list-leave"
+              className="pl-2"
+            >
+              Leave List
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+          </NavDropdown>
 
-          <Nav.Link
-            as={Link}
-            to="/add-attendance"
-            className="text-primary pl-2"
+          <NavDropdown
+            title="Attendance"
+            id="nav-dropdown"
+            className="color-primary"
           >
-            Add Attendance
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="/list-attendance"
-            className="text-primary pl-2"
+            <NavDropdown.Item
+              eventKey="4.1"
+              as={Link}
+              to="/add-attendance"
+              className="pl-2"
+              style={{ color: "tomato" }}
+            >
+              Add Attendance
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item
+              eventKey="4.2"
+              as={Link}
+              to="/list-attendance"
+              className="pl-2"
+            >
+              Attendance List
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+          </NavDropdown>
+
+          <NavDropdown
+            title="Timecard"
+            id="nav-dropdown"
+            className="color-primary"
           >
-            List Attendance
-          </Nav.Link>
+            <NavDropdown.Item
+              eventKey="4.1"
+              as={Link}
+              to="/add-timecard"
+              className="pl-2"
+              style={{ color: "tomato" }}
+            >
+              Add TimeCard
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item
+              eventKey="4.2"
+              as={Link}
+              to="/list-timecard"
+              className="pl-2"
+            >
+              TimeCard List
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+          </NavDropdown>
 
-          <Nav.Link as={Link} to="/apply-leave" className="text-primary pl-2">
-            Apply Leave
-          </Nav.Link>
-          <Nav.Link as={Link} to="/list-leave" className="text-primary pl-2">
-            List Leave
-          </Nav.Link>
-
-          <Nav.Link as={Link} to="/add-timecard" className="text-primary pl-2">
-            Add Timecard
-          </Nav.Link>
-          <Nav.Link as={Link} to="/list-timecard" className="text-primary pl-2">
-            List Timecard
-          </Nav.Link>
+          <NavDropdown
+            title="Supervisor"
+            id="nav-dropdown"
+            className="color-primary"
+          >
+            <NavDropdown.Item
+              eventKey="4.1"
+              as={Link}
+              to="/create-supervisor"
+              className="pl-2"
+              style={{ color: "tomato" }}
+            >
+              Create Supervisor
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item
+              eventKey="4.2"
+              as={Link}
+              to="/list-supervisor"
+              className="pl-2"
+            >
+              Supervisor List
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+          </NavDropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
